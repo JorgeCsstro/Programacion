673d5971e078acf569a383ec54d5e5e76b5947e9
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-10-26 16:24:36 +0200

    TEMPLATE

---
+++ Template.java
@@ -0,0 +1,25 @@
+public class Template {
+
+	public static void main (String[] args) {
+
+        /*VARIABLES: */
+
+
+
+        /*ATAJOS: */
+
+        Scanner sc = new Scanner(System.in);
+
+        /*CODIGO: */
+
+		
+
+
+
+
+
+
+        
+
+	}
+}
