bab1b9442ba62f28d9cf42f17e08047469fc27dc
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-10-27 16:50:07 +0200

    ARRAY INFO

---
+++ EjemploArr.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EjemploArr {
+
+	public static void main (String[] args) {
+
+        /*VARIABLES: */
+
+        int[][] a = {{1,2,3},{4,5,6},{7,8,9}};
+
+        /*ATAJOS: */
+
+        /*CODIGO: */
+
+        for (int i = 0;i <=2;i++){
+                for (int j = 0;j <= 2;j++){
+
+                     System.out.println(i + "," + j + ":" + a[i][j]);
+
+                }
+
+
+        }
+
+	
+        
+
+
+
+
+
+        
+
+	}
+}
