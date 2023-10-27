5bd5ae3d102a04e9f68fb1827a55bc60e213a953
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-10-27 16:43:33 +0200

    ARRAY

---
+++ Ejercicio1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main (String[] args) {
+
+        	/*VARIABLES: */
+
+        	int[] ar = new int[15];
+
+        	int i;
+
+        	/*ATAJOS: */
+
+        	Scanner sc = new Scanner(System.in);
+
+        	/*CODIGO: */
+
+	        System.out.println("Introduce 15 números: ");
+	
+	        for (i = 0; i < ar.length-1; i++) {
+	
+	            System.out.print("Número " + (i+1) + ": ");
+	            ar[i] = sc.nextInt();
+	            
+	        }
+	
+	        for (i = 0; i < ar.length-1;i++){
+	
+	            System.out.println(ar[i]);
+	
+	        }
+	}
+}
